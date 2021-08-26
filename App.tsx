f399eb5546678862831d2f64c7221a1d68235c70
andi.rayka <andi.rayka@gmail.com>
2021-08-26 14:06:07 +0700

    add login with google and add upload key

--- App.tsx
+++ App.tsx
@@ -6,6 +6,9 @@ import 'dayjs/locale/id';
 import dayjs from 'dayjs';
 dayjs.locale('id');
 
+// Sign in with google
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
 import React, {FC} from 'react';
 import {enableScreens} from 'react-native-screens';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -15,6 +18,20 @@ import AppContextProvider from '@context';
 
 // to indicate that inactive screens should be detached from the view hierarchy to save memory
 enableScreens();
+
+// Initialization of siginin with google feature
+GoogleSignin.configure({
+  // scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+  webClientId:
+    '508384095334-uqi7vshb8v3krm2r7bacerjtpdfm82fa.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+  offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  // hostedDomain: '', // specifies a hosted domain restriction
+  // loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+  forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+  // accountName: '', // [Android] specifies an account name on the device that should be used
+  // iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+  // googleServicePlistPath: '', // [iOS] optional, if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
+});
 
 const App: FC = () => {
   return (
--- src/screens/LoginGeneral.tsx
+++ src/screens/LoginGeneral.tsx
@@ -7,19 +7,45 @@ import {
   StyleSheet,
 } from 'react-native';
 import {AuthContext} from '@context/AuthContext';
-import {Button, Subheading, Title} from 'react-native-paper';
+import {Button, Subheading, Text, Title} from 'react-native-paper';
 import {StackScreenProps} from '@react-navigation/stack';
 import {AppStackParamList} from '@routes/RouteTypes';
 import {color, dimens} from '@constants';
 import {LogoLesinAja} from '@assets';
+import {
+  GoogleSigninButton,
+  GoogleSignin,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 
 type ScreenProps = StackScreenProps<AppStackParamList, 'LoginGeneral'>;
 // Login screen for Parent and Tutor
 export const LoginGeneral: FC<ScreenProps> = ({navigation}) => {
   const {loginParent} = useContext(AuthContext);
 
-  const onPressLogin = () => {
-    loginParent();
+  const onPressLogin = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log({userInfo});
+      // this.setState({userInfo});
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+        console.log('user cancelled the login flow');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+        console.log('operation (e.g. sign in) is in progress already');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+        console.log('play services not available or outdated');
+      } else {
+        console.log(error);
+        // some other error happened
+      }
+    }
+
+    // loginParent();
   };
 
   return (
@@ -35,6 +61,14 @@ export const LoginGeneral: FC<ScreenProps> = ({navigation}) => {
         />
         <Title>Selamat Datang!</Title>
         <Subheading>Masuk dengan akun Google untuk melanjutkan.</Subheading>
+
+        <GoogleSigninButton
+          style={{height: 48}}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={onPressLogin}
+          disabled={false}
+        />
       </ScrollView>
     </SafeAreaView>
   );
